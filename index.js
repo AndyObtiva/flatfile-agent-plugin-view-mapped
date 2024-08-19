e192b01d4b3c2035495a2f18550e025fdd7943f8
Andy Maleh <andy.am@gmail.com>
2024-08-19 11:01:48 -0400

    Add @flatfile/plugin-xlsx-extractor plugin

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 /**
- * This code was inspired by Flatfile's Custom App Tutorial
- * https://flatfile.com/docs/apps/custom
  *
  * To see all of Flatfile's code examples go to: https://github.com/FlatFilers/flatfile-docs-kitchen-sink
  */
 
+import { ExcelExtractor } from '@flatfile/plugin-xlsx-extractor'
 import { viewMappedPlugin } from '@flatfile/plugin-view-mapped'
 
 export default function(listener) {
+  listener.use(ExcelExtractor())
+  
   // this plugin hides all unmapped columns, making new Flatfile Platform behave like Flatfile Portal v2
   listener.use(viewMappedPlugin())
 }
+ 
